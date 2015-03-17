eaf93e80fd3880fd4186c93cfcb9ae04a1087f0d
amstut_a <arthur.amstutz@epitech.eu>
2015-03-17 18:55:55 +0100

    clean

--- guiSDL.cpp
+++ guiSDL.cpp
@@ -1,17 +1,26 @@
+//
+// guiSDL.cpp for guiSDL in /home/amstuta/rendu/cpp_nibbler
+//
+// Made by arthur
+// Login   <amstuta@epitech.net>
+//
+// Started on  Tue Mar 17 18:55:33 2015 arthur
+// Last update Tue Mar 17 18:55:39 2015 arthur
+//
+
 #include "guiSDL.hpp"
 
-guiSDL::guiSDL()
+guiSDL::guiSDL():
+  IGui::IGui()
 {
-
 }
 
 guiSDL::~guiSDL()
 {
-
 }
 
 void guiSDL::initGui()
 {
-
+  
 }
 
